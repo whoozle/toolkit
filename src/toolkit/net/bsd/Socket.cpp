7ff39f6a282664592fa4b512f0e5a183ff683d21
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2018-03-10 14:10:23 +0000

    added bsd socket api

--- src/toolkit/net/bsd/Socket.cpp
+++ src/toolkit/net/bsd/Socket.cpp
@@ -2,7 +2,9 @@
 #include <toolkit/io/SystemException.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <sys/sendfile.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 TOOLKIT_SUBNS2_BEGIN(net, bsd)
 
@@ -18,14 +20,60 @@ TOOLKIT_SUBNS2_BEGIN(net, bsd)
 	}
 
 	void Socket::GetOption(int level, int optname, void *optval, socklen_t *optlen)
-	{
-		SYSTEM_CALL(getsockopt(_socket, level, optname, optval, optlen));
-	}
+	{ SYSTEM_CALL(getsockopt(_socket, level, optname, optval, optlen)); }
 
 	void Socket::SetOption(int level, int optname, const void *optval, socklen_t optlen)
+	{ SYSTEM_CALL(setsockopt(_socket, level, optname, optval, optlen)); }
+
+	int Socket::GetFlags() const
 	{
-		SYSTEM_CALL(setsockopt(_socket, level, optname, optval, optlen));
+		int flags = fcntl(_socket, F_GETFL, 0);
+		if (flags == -1)
+			throw io::SystemException("fcntl(socketfd, F_GETFL, 0)");
+		return flags;
 	}
+
+	void Socket::SetFlags(int flags)
+	{ SYSTEM_CALL(fcntl(_socket, F_SETFL, flags)); }
+
+	bool Socket::GetNonBlocking() const
+	{ return GetFlags() & O_NONBLOCK; }
+
+	void Socket::SetNonBlocking(bool enabled)
+	{
+		int flags = GetFlags();
+
+		if (enabled)
+			flags |= O_NONBLOCK;
+		else
+			flags &= ~O_NONBLOCK;
+
+		SetFlags(flags);
+	}
+
+	ssize_t Socket::Send(const void *buf, size_t len, int flags)
+	{ SYSTEM_CALL_RETURN(send(_socket, buf, len, flags)); }
+
+	ssize_t Socket::SendTo(const void *buf, size_t len, int flags,
+                      const struct sockaddr *dest_addr, socklen_t addrlen)
+	{ SYSTEM_CALL_RETURN(sendto(_socket, buf, len, flags, dest_addr, addrlen)); }
+
+	ssize_t Socket::SendMsg(const struct msghdr *msg, int flags)
+	{ SYSTEM_CALL_RETURN(sendmsg(_socket, msg, flags)); }
+
+	ssize_t Socket::Recv(void *buf, size_t len, int flags)
+	{ SYSTEM_CALL_RETURN(recv(_socket, buf, len, flags)); }
+
+	ssize_t Socket::RecvFrom(void *buf, size_t len, int flags,
+                        struct sockaddr *src_addr, socklen_t *addrlen)
+	{ SYSTEM_CALL_RETURN(recvfrom(_socket, buf, len, flags, src_addr, addrlen)); }
+
+	ssize_t Socket::RecvMsg(struct msghdr *msg, int flags)
+	{ SYSTEM_CALL_RETURN(recvmsg(_socket, msg, flags)); }
+
+	ssize_t Socket::SendFile(int in_fd, off_t *offset, size_t count)
+	{ SYSTEM_CALL_RETURN(sendfile(_socket, in_fd, offset, count)); }
+
 
 
 TOOLKIT_SUBNS2_END
--- src/toolkit/net/bsd/Socket.h
+++ src/toolkit/net/bsd/Socket.h
@@ -16,6 +16,28 @@ TOOLKIT_SUBNS2_BEGIN(net, bsd)
 
 		void GetOption(int level, int optname, void *optval, socklen_t *optlen);
 		void SetOption(int level, int optname, const void *optval, socklen_t optlen);
+
+		int GetFlags() const;
+		void SetFlags(int flags);
+
+		bool GetNonBlocking() const;
+		void SetNonBlocking(bool enabled);
+
+		void Connect(const struct sockaddr *addr, socklen_t addrlen);
+		void Bind(const struct sockaddr *addr, socklen_t addrlen);
+		void Listen(int backlog);
+
+		ssize_t Send(const void *buf, size_t len, int flags);
+		ssize_t SendTo(const void *buf, size_t len, int flags,
+                      const struct sockaddr *dest_addr, socklen_t addrlen);
+		ssize_t SendMsg(const struct msghdr *msg, int flags);
+
+		ssize_t Recv(void *buf, size_t len, int flags);
+		ssize_t RecvFrom(void *buf, size_t len, int flags,
+                        struct sockaddr *src_addr, socklen_t *addrlen);
+		ssize_t RecvMsg(struct msghdr *msg, int flags);
+
+		ssize_t SendFile(int in_fd, off_t *offset, size_t count);
 	};
 
 TOOLKIT_SUBNS2_END
