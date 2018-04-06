2b55b453a811223a4d319eeb788b6ce6fc5706d8
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2018-04-06 17:04:08 +0100

    added memory stream stub

---
+++ src/toolkit/io/MemoryStream.h
@@ -0,0 +1,36 @@
+#ifndef TOOLKIT_BYTEDATASTREAM_H
+#define TOOLKIT_BYTEDATASTREAM_H
+
+#include <toolkit/io/IStream.h>
+#include <string.h>
+#include <vector>
+
+TOOLKIT_NS_BEGIN
+namespace io
+{
+
+	class MemoryOutputStream : public IOutputStream
+	{
+		using Storage = std::vector<u8>;
+		Storage _data;
+
+	public:
+		MemoryOutputStream(size_t initialCapacity = 4096): _data() { _data.reserve(initialCapacity); }
+
+		size_t Write(ConstByteData src) override
+		{
+			size_t offset = _data.size();
+			size_t size = src.size();
+			_data.resize(offset + size);
+			memcpy(_data.data() + offset, src.data, size);
+			return size;
+		}
+
+		Storage && GetData()
+		{ return _data; }
+	};
+
+}
+TOOLKIT_NS_END
+
+#endif
