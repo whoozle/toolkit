487947e2617dda9c025d7b58c21a937d23796288
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2018-06-26 18:07:59 +0100

    added DoubleSize template

---
+++ src/toolkit/core/type_traits.h
@@ -0,0 +1,23 @@
+#ifndef TOOLKIT_TYPE_TRAITS
+#define TOOLKIT_TYPE_TRAITS
+
+#include <toolkit/core/types.h>
+
+TOOLKIT_NS_BEGIN
+
+
+	template<typename T>
+	struct DoubleSize;
+
+	template<> struct DoubleSize<u8>  { using Type = u16; };
+	template<> struct DoubleSize<u16> { using Type = u32; };
+	template<> struct DoubleSize<u32> { using Type = u64; };
+
+	template<> struct DoubleSize<s8>  { using Type = s16; };
+	template<> struct DoubleSize<s16> { using Type = s32; };
+	template<> struct DoubleSize<s32> { using Type = s64; };
+
+TOOLKIT_NS_END
+
+#endif
+
