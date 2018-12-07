e998a6d75478789bf6da4fb6be1120a607258a18
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2018-12-07 16:53:47 +0000

    added Event

---
+++ src/toolkit/io/Event.cpp
@@ -0,0 +1,52 @@
+#include <toolkit/io/Event.h>
+#include <toolkit/io/SystemException.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <limits.h>
+
+TOOLKIT_NS_BEGIN
+namespace io
+{
+	Event::Event(): _active(false)
+	{
+		int fd[2];
+		if (pipe(fd) == -1)
+			throw SystemException("pipe");
+		_rd = fd[0];
+		_wd = fd[1];
+	}
+
+	Event::~Event()
+	{
+		close(_rd);
+		close(_wd);
+	}
+
+	void Event::Signal()
+	{
+		std::lock_guard<decltype(_mutex)> l(_mutex);
+		if (_active)
+			return;
+
+		u8 value = 0;
+		if (write(_wd, &value, 1) != 1)
+			throw SystemException("write");
+		_active = true;
+	}
+
+	void Event::Cancel()
+	{
+		std::lock_guard<decltype(_mutex)> l(_mutex);
+		if (!_active)
+			return;
+		u8 buf[PIPE_BUF];
+		ssize_t r = read(_rd, buf, sizeof(buf));
+		if (r == -1)
+			throw SystemException("read");
+		if (r != 1)
+			throw std::logic_error("more than one byte in pipe");
+		_active = false;
+	}
+}
+TOOLKIT_NS_END
---
+++ src/toolkit/io/Event.h
@@ -0,0 +1,35 @@
+#ifndef TOOLKIT_EVENT_H
+#define TOOLKIT_EVENT_H
+
+#include <toolkit/io/IPollable.h>
+#include <toolkit/core/Noncopyable.h>
+#include <toolkit/core/types.h>
+#include <memory>
+#include <mutex>
+
+TOOLKIT_NS_BEGIN
+namespace io
+{
+
+	class Event : public IPollable, public Noncopyable
+	{
+		std::mutex	_mutex;
+		bool		_active;
+		int			_rd, _wd;
+
+	public:
+		Event();
+		~Event();
+
+		int GetFileDescriptor() const override
+		{ return _rd; }
+
+		void Signal();
+		void Cancel();
+	};
+	DECLARE_PTR(Event);
+
+}
+TOOLKIT_NS_END
+
+#endif
