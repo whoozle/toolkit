1f18940529cc9d36eb66737a4a0e7802362c8da4
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2019-05-19 18:27:01 +0100

    added GetHash(...), CombineHash(...) and CombineHashRange(...)

---
+++ src/toolkit/core/Hash.h
@@ -0,0 +1,38 @@
+#ifndef TOOLKIT_CORE_HASH_H
+#define TOOLKIT_CORE_HASH_H
+
+#include <cstddef>
+#include <functional>
+
+namespace TOOLKIT_NS
+{
+
+	template<typename IteratorType>
+	std::size_t CombineHashRange(IteratorType begin, IteratorType end)
+	{
+		std::size_t result = 17;
+		for(; begin != end; ++begin)
+			result = result * 31 + *begin;
+		return result;
+	}
+
+	template<typename ... Args>
+	std::size_t GetHash(Args ... args)
+	{
+		std::array<std::size_t, sizeof ... (args)> hashes = \
+			{ std::hash<typename std::decay<Args>::type>()(args) ... };
+		return CombineHashRange(hashes.begin(), hashes.end());
+	}
+
+	template<typename ... Args>
+	std::size_t CombineHash(Args ... args)
+	{
+		std::array<std::size_t, sizeof ... (args)> hashes = \
+			{ std::hash<typename std::decay<Args>::type>()(args) ... };
+
+		return CombineHashRange(hashes.begin(), hashes.end());
+	}
+
+}
+
+#endif
