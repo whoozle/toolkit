71f8a8bd978562e7415a1290f5b3d0575a0b3ce0
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2019-06-18 00:37:32 +0100

    added Framebuffer object wrapper

---
+++ src/toolkit/raster/gl/FrameBuffer.cpp
@@ -0,0 +1,23 @@
+#include <toolkit/raster/gl/FrameBuffer.h>
+#include <toolkit/raster/gl/Utils.h>
+
+namespace TOOLKIT_NS { namespace gl
+{
+	FrameBuffer::FrameBuffer()
+	{
+		glGenFramebuffers(1, &_fbo);
+		CheckGLError();
+	}
+
+	FrameBuffer::~FrameBuffer()
+	{
+		glDeleteFramebuffers(1, &_fbo);
+	}
+
+	void FrameBuffer::Bind(GLenum target)
+	{
+		glBindFramebuffer(target, _fbo);
+		CheckGLError();
+	}
+
+}}
---
+++ src/toolkit/raster/gl/FrameBuffer.h
@@ -0,0 +1,21 @@
+#ifndef TOOLKIT_GLES_FRAMEBUFFER_H
+#define TOOLKIT_GLES_FRAMEBUFFER_H
+
+#include <toolkit/raster/gl/Utils.h>
+#include <toolkit/core/Noncopyable.h>
+
+namespace TOOLKIT_NS { namespace gl
+{
+	class FrameBuffer : public Noncopyable
+	{
+		GLuint		_fbo;
+
+	public:
+		FrameBuffer();
+		~FrameBuffer();
+
+		void Bind(GLenum target);
+	};
+}}
+
+#endif
