e408f77b5297c30fd37bb0682b73780777edac07
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2019-07-11 22:38:17 +0100

    cleaned up thread pool, replaced printf with logger

---
+++ src/toolkit/core/ThreadPool.cpp
@@ -0,0 +1,46 @@
+#include <toolkit/core/ThreadPool.h>
+
+namespace TOOLKIT_NS
+{
+	log::Logger	ThreadPool::Thread::_log("thread");
+	log::Logger	ThreadPool::_log("thread-pool");
+
+	void ThreadPool::Thread::Run()
+	{
+		_log.Debug() << "pooled thread started";
+		std::unique_lock<std::mutex> l(_pool->_mutex);
+		while(_pool->_running)
+		{
+			_pool->_condition.wait(l);
+			while (!_pool->_tasks.empty())
+			{
+				Task task(std::move(_pool->_tasks.front()));
+				_pool->_tasks.pop();
+				if (!_pool->_tasks.empty())
+					_pool->_condition.notify_all();
+				l.unlock();
+				try { task(); } catch(const std::exception &ex)
+				{ _log.Warning() << "error in thread pool task: " << ex.what(); }
+				l.lock();
+			}
+		}
+	}
+
+	ThreadPool::ThreadPool(size_t num): _running(true)
+	{
+		_log.Debug() << "starting thread pool of " << num << " threads";
+		while(num--)
+			_threads.push_back(std::make_shared<Thread>(this));
+	}
+
+	ThreadPool::~ThreadPool()
+	{
+		{
+			std::lock_guard<std::mutex> l(_mutex);
+			_running = false;
+			_condition.notify_all();
+		}
+		_threads.clear();
+	}
+
+}
--- src/toolkit/core/ThreadPool.h
+++ src/toolkit/core/ThreadPool.h
@@ -1,55 +1,41 @@
-#ifndef PURE_CORE_THREAD_POOL
-#define PURE_CORE_THREAD_POOL
+#ifndef TOOLKIT_THREAD_POOL
+#define TOOLKIT_THREAD_POOL
 
+#include <toolkit/log/Logger.h>
 #include <toolkit/core/Noncopyable.h>
-#include <thread>
+#include <toolkit/core/types.h>
 #include <condition_variable>
 #include <future>
-#include <queue>
+#include <memory>
 #include <mutex>
-#include <stdio.h>
+#include <thread>
+#include <queue>
 
 namespace TOOLKIT_NS
 {
-
 	class ThreadPool : Noncopyable
 	{
 		using Task = std::function<void ()>;
 
-		struct Thread : Noncopyable
+		class Thread : Noncopyable
 		{
-			ThreadPool *	_pool;
-			std::thread		_thread;
+			static log::Logger	_log;
+			ThreadPool *		_pool;
+			std::thread			_thread;
 
-			Thread(ThreadPool * pool): _pool(pool), _thread(&Thread::Run, this) { }
-			void Run()
-			{
-				printf("thread pool thread started\n");
-				std::unique_lock<std::mutex> l(_pool->_mutex);
-				while(_pool->_running)
-				{
-					_pool->_condition.wait(l);
-					while (!_pool->_tasks.empty())
-					{
-						Task task(std::move(_pool->_tasks.front()));
-						_pool->_tasks.pop();
-						if (!_pool->_tasks.empty())
-							_pool->_condition.notify_all();
-						l.unlock();
-						try { task(); } catch(const std::exception &ex)
-						{ fprintf(stderr, "error in thread pool task: %s\n", ex.what()); }
-						l.lock();
-					}
-				}
-			}
+		public:
+			Thread(ThreadPool * pool):
+				_pool(pool), _thread(&Thread::Run, this)
+			{ }
 
 			~Thread()
-			{
-				_thread.join();
-			}
+			{ _thread.join(); }
+
+			void Run();
 		};
 		DECLARE_PTR(Thread);
 
+		static log::Logger		_log;
 		std::mutex				_mutex;
 		bool					_running;
 		std::condition_variable _condition;
@@ -57,28 +43,16 @@ namespace TOOLKIT_NS
 		std::vector<ThreadPtr>	_threads;
 
 	public:
-		ThreadPool(): ThreadPool(std::thread::hardware_concurrency()) { }
-		ThreadPool(unsigned num): _running(true)
-		{
-			printf("starting thread pool of %u threads\n", num);
-			while(num--)
-				_threads.push_back(std::make_shared<Thread>(this));
-		}
-		~ThreadPool()
-		{
-			{
-				std::lock_guard<std::mutex> l(_mutex);
-				_running = false;
-				_condition.notify_all();
-			}
-			_threads.clear();
-		}
+		ThreadPool(size_t num);
+		ThreadPool(): ThreadPool(std::thread::hardware_concurrency())
+		{ }
+		~ThreadPool();
 
 		template<typename T>
-		void AddTask(T callable)
+		void AddTask(T && callable)
 		{
 			std::lock_guard<std::mutex> l(_mutex);
-			_tasks.emplace(callable);
+			_tasks.emplace(std::forward<T>(callable));
 			_condition.notify_one();
 		}
 	};
