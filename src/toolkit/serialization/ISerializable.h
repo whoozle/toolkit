8da418d539afa74b5c0549b62e927066cd68c72b
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2019-07-18 22:19:04 +0100

    added ISerialisableType interface + GetType() method

--- src/toolkit/serialization/ISerializable.h
+++ src/toolkit/serialization/ISerializable.h
@@ -1,13 +1,22 @@
 #ifndef TOOLKIT_SERIALIZATION_ISERIALIZABLE_H
 #define TOOLKIT_SERIALIZATION_ISERIALIZABLE_H
 
-#include <toolkit/core/core.h>
+#include <toolkit/core/types.h>
+#include <memory>
 
 namespace TOOLKIT_NS
 {
+	struct ISerializableType
+	{
+		virtual ~ISerializableType() = default;
+		virtual const std::string GetType() const = 0;
+	};
+	DECLARE_CONST_PTR(ISerializableType);
+
 	struct ISerializable
 	{
 		virtual ~ISerializable() = default;
+		virtual ISerializableTypeConstPtr GetType() const = 0;
 	};
 }
 
