a39221402a0bb91b10ed7f3df37b04f0a5ebe10d
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2019-07-19 22:32:53 +0100

    added stub of Record<...>

---
+++ src/toolkit/serialization/Record.h
@@ -0,0 +1,14 @@
+#ifndef TOOLKIT_SERIALIZATION_RECORD_H
+#define TOOLKIT_SERIALIZATION_RECORD_H
+
+namespace TOOLKIT_NS { namespace serialization
+{
+	template <typename ... Types>
+	struct Record
+	{
+		
+	};
+
+}}
+
+#endif
