c3ff241d168a9cd315841f18d2269bfbf70270aa
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2019-09-16 21:38:34 +0100

    changed s8 to char

--- src/toolkit/core/StringRef.h
+++ src/toolkit/core/StringRef.h
@@ -2,15 +2,19 @@
 #define TOOLKIT_CORE_STRINGREF
 
 #include <toolkit/core/Buffer.h>
+#include <string>
 
 namespace TOOLKIT_NS
 {
-	class StringRef : public BasicBuffer<s8>
+	class StringRef : public BasicBuffer<char>
     {
-        using super = BasicBuffer<s8>;
+        using super = BasicBuffer<char>;
 
     public:
         using super::super;
+
+		std::string ToString() const
+		{ return std::string(static_cast<const char *>(data()), size()); }
     };
 }
 
