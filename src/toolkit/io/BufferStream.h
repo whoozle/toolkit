2393b6d91950d84f0d2a8adbefde96b1afef0fff
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2021-09-12 22:52:33 +0100

    make buffer streams seekable

--- src/toolkit/io/BufferStream.h
+++ src/toolkit/io/BufferStream.h
@@ -8,16 +8,51 @@
 
 namespace TOOLKIT_NS { namespace io
 {
+	class BufferStreamBase : virtual public ISeekable
+	{
+	protected:
+		off_t _offset;
+
+		virtual off_t GetBufferSize() const = 0;
+
+	public:
+		BufferStreamBase(): _offset(0)
+		{ }
+
+		off_t Seek(off_t offset, SeekMode mode = SeekMode::Begin) override
+		{
+			switch (mode)
+			{
+			case SeekMode::Begin:
+				_offset = offset;
+				break;
+			case SeekMode::Current:
+				_offset += offset;
+				break;
+			case SeekMode::End:
+				_offset = GetBufferSize() + offset;
+				break;
+			}
+			return _offset;
+		}
+
+		off_t Tell() override
+		{ return _offset; }
+	};
 
 	class BufferInputStream :
-		public virtual IInputStream,
+		public BufferStreamBase,
+		public virtual ISeekableInputStream,
 		public virtual IBufferedInputStream
 	{
 		ConstBuffer		_src;
-		size_t			_offset;
+
+	private:
+		off_t GetBufferSize() const override
+		{ return _src.size(); }
 
 	public:
-		BufferInputStream(ConstBuffer src): _src(src), _offset(0) { }
+		BufferInputStream(ConstBuffer src): _src(src) { }
 
 		size_t Read(Buffer dst) override
 		{
@@ -27,6 +62,7 @@ namespace TOOLKIT_NS { namespace io
 			_offset += n;
 			return n;
 		}
+
         ConstBuffer GetReadBuffer() const override
 		{ return ConstBuffer(_src, _offset); }
 
@@ -40,14 +76,18 @@ namespace TOOLKIT_NS { namespace io
 	};
 
 	class BufferOutputStream :
-		public virtual IOutputStream,
+		public BufferStreamBase,
+		public virtual ISeekableOutputStream,
 		public virtual IBufferedOutputStream
 	{
+	private:
 		Buffer			_dst;
-		size_t			_offset;
+
+		off_t GetBufferSize() const override
+		{ return _dst.size(); }
 
 	public:
-		BufferOutputStream(Buffer dst): _dst(dst), _offset(0) { }
+		BufferOutputStream(Buffer dst): _dst(dst) { }
 
 		size_t Write(ConstBuffer src) override
 		{
@@ -70,19 +110,6 @@ namespace TOOLKIT_NS { namespace io
 
         void WriteSkip(size_t bytes) override
 		{ WriteComplete(bytes); }
-	};
-
-	class BufferBidirectionalStream :
-		public BufferInputStream,
-		public BufferOutputStream,
-		public virtual IBidirectionalStream,
-		public virtual IBufferedBidirectionalStream
-	{
-	public:
-		BufferBidirectionalStream(Buffer buffer):
-			BufferInputStream(buffer),
-			BufferOutputStream(buffer)
-		{ }
 	};
 
 }}
