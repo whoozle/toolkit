387b8114d934a8e60342da74e51ef48fa33fa1c3
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2021-09-26 23:22:17 +0100

    reimplement buffered stream

--- src/toolkit/io/BufferedStream.h
+++ src/toolkit/io/BufferedStream.h
@@ -3,64 +3,32 @@
 
 #include <toolkit/io/IStream.h>
 #include <toolkit/core/Noncopyable.h>
-#include <vector>
+#include <toolkit/core/ByteArray.h>
 
 namespace TOOLKIT_NS { namespace io
 {
-
-	class BufferedStream : public IBufferedStream, Noncopyable
+	class BufferedStreamBase : public virtual IFlushable
 	{
-		IStoragePtr		_stream;
-		std::vector<u8> _buffer;
-		size_t			_writeBufferOffset;
+	protected:
+		ByteArray 	_buffer;
+		size_t		_offset;
 
 	public:
-		BufferedStream(IStoragePtr stream, size_t bufferSize): _stream(stream), _buffer(bufferSize), _writeBufferOffset(0)
+		BufferedStreamBase(size_t bufferSize): _buffer(bufferSize), _offset(0)
 		{ }
+	};
 
-		~BufferedStream()
-		{ Flush(); }
+	class BufferedOutputStream :
+		public virtual BufferedStreamBase,
+		public virtual IOutputStream
+	{
+	protected:
+		IOutputStreamPtr	_stream;
 
-		void Flush() override
-		{
-			if (_writeBufferOffset)
-			{
-				if (_stream->Write(ConstBuffer(_buffer.data(), _writeBufferOffset)) != _writeBufferOffset)
-					throw Exception("short write");
-				_writeBufferOffset = 0;
-			}
-		}
-
-		size_t Read(Buffer data) override
-		{
-			Flush();
-			return _stream->Read(data);
-		}
-
-		off_t Seek(off_t offset, SeekMode mode) override
-		{
-			Flush();
-			return _stream->Seek(offset, mode);
-		}
-
-		void Truncate(size_t size) override
-		{
-			Flush();
-			_stream->Truncate(size);
-		}
-
-		void Sync(SyncMode mode) override
-		{
-			Flush();
-			_stream->Sync(mode);
-		}
-
-		off_t Tell() override
-		{
-			off_t streamOffset = _stream->Tell();
-			size_t remainingData = 0; //readbuffer offset
-			return streamOffset - remainingData + _writeBufferOffset;
-		}
+	public:
+		BufferedOutputStream(IOutputStreamPtr stream, size_t bufferSize):
+			BufferedStreamBase(bufferSize), _stream(stream)
+		{ }
 
 		size_t Write(ConstBuffer data) override
 		{
@@ -68,16 +36,121 @@ namespace TOOLKIT_NS { namespace io
 			auto remainingSrcData = data.GetSize();
 			while(remainingSrcData != 0)
 			{
-				auto r = std::min(remainingSrcData, _buffer.size() - _writeBufferOffset);
-				std::copy(srcData, srcData + r, _buffer.data() + _writeBufferOffset);
+				auto r = std::min(remainingSrcData, _buffer.size() - _offset);
+				std::copy(srcData, srcData + r, _buffer.data() + _offset);
 				srcData += r;
-				_writeBufferOffset += r;
+				_offset += r;
 				remainingSrcData -= r;
-				if (_writeBufferOffset == _buffer.size())
+				if (_offset == _buffer.size())
 					Flush();
 			}
 			return data.GetSize();
 		}
+
+		void Flush() override
+		{
+			if (_offset)
+			{
+				if (_stream->Write(ConstBuffer(_buffer.data(), 0, _offset)) != _offset)
+					throw Exception("short write");
+				_offset = 0;
+			}
+		}
+
+		~BufferedOutputStream()
+		{ BufferedOutputStream::Flush(); }
+	};
+
+	class BufferedSeekableStream :
+		public virtual ISeekable
+	{
+		ISeekablePtr _stream;
+
+	public:
+		BufferedSeekableStream(const ISeekablePtr & stream):
+			_stream(stream)
+		{ }
+
+		off_t Seek(off_t offset, SeekMode mode = SeekMode::Begin) override
+		{
+			auto flushable = std::dynamic_pointer_cast<IFlushable>(_stream);
+			if (flushable)
+				flushable->Flush();
+			//FIXME: relative seek does not work
+			return _stream->Seek(offset, mode);
+		}
+
+		off_t Tell() override
+		{ return _stream->Tell(); }
+	};
+
+	class BufferedInputStream :
+		public virtual BufferedStreamBase,
+		public virtual IInputStream
+	{
+	protected:
+		IInputStreamPtr		_stream;
+		size_t				_bufferSize;
+
+	public:
+		BufferedInputStream(IInputStreamPtr stream, size_t bufferSize):
+			BufferedStreamBase(bufferSize), _stream(stream)
+		{ _bufferSize = 0; }
+
+		size_t Read(o1::Buffer data) override
+		{
+			auto dstData = data.GetPointer();
+			size_t dstPos = 0;
+			while(dstPos != data.size())
+			{
+				if (_offset == _bufferSize)
+				{
+					_bufferSize = _stream->Read(_buffer);
+					_offset = 0;
+				}
+
+				auto r = std::min(data.size() - dstPos, _bufferSize - _offset);
+				auto srcData = _buffer.data() + _offset;
+				std::copy(srcData, srcData + r, dstData);
+				dstData += r;
+				dstPos += r;
+				_offset += r;
+			}
+			return data.GetSize();
+		}
+
+		void Flush() override
+		{
+			_offset = 0;
+			_bufferSize = 0;
+		}
+	};
+
+	class BufferedStream:
+		public BufferedInputStream,
+		public BufferedOutputStream,
+		public BufferedSeekableStream,
+		public virtual IStorage
+	{
+		IStoragePtr _storage;
+
+	public:
+		BufferedStream(const IStoragePtr & storage, size_t bufferSize):
+			BufferedStreamBase(bufferSize),
+			BufferedInputStream(std::static_pointer_cast<IInputStream>(storage), bufferSize),
+			BufferedOutputStream(std::static_pointer_cast<IOutputStream>(storage), bufferSize),
+			BufferedSeekableStream(std::static_pointer_cast<ISeekable>(storage)),
+			_storage(storage)
+		{ }
+
+		void Sync(io::SyncMode mode) override
+		{ _storage->Sync(mode); }
+
+		void Truncate(size_t size) override
+		{ _storage->Truncate(size); }
+
+		void Flush() override
+		{ BufferedOutputStream::Flush(); }
 	};
 
 }}
