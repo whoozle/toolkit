d2072cb6a998d2eafd80fbc2dddf8532ede56cf1
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2021-12-30 00:48:47 +0000

    add FromString

---
+++ src/toolkit/text/FromString.h
@@ -0,0 +1,65 @@
+#ifndef TOOLKIT_TEXT_FROMSTRING_H
+#define TOOLKIT_TEXT_FROMSTRING_H
+
+#include <toolkit/text/String.h>
+#include <toolkit/core/Exception.h>
+#include <string>
+
+namespace TOOLKIT_NS { namespace text
+{
+	namespace impl
+	{
+		template<typename T>
+		struct FromString;
+
+		template<> struct FromString<int>
+		{
+			static int Parse(const std::string & str)
+			{ return std::stoi(str); }
+		};
+
+		template<> struct FromString<long>
+		{
+			static long Parse(const std::string & str)
+			{ return std::stol(str); }
+		};
+
+		template<> struct FromString<long long>
+		{
+			static long long Parse(const std::string & str)
+			{ return std::stoll(str); }
+		};
+
+		template<> struct FromString<std::string>
+		{
+			static std::string Parse(const std::string & str)
+			{ return str; }
+		};
+
+		template<> struct FromString<std::wstring>
+		{
+			static std::wstring Parse(const std::string & str)
+			{ return FromUtf8(str); }
+		};
+
+		template<> struct FromString<bool>
+		{
+			static bool Parse(const std::string & str)
+			{
+				if (str == "yes" || str == "true")
+					return true;
+				else if (str == "no" || str == "false")
+					return false;
+				else
+				 	throw Exception("invalid boolean value " + str);
+			}
+		};
+	}
+
+	template <typename T>
+	T FromString(const std::string & str)
+	{ return impl::FromString<T>::Parse(str); }
+
+}}
+
+#endif
