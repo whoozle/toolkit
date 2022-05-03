8a3b79bba3add268639c14ab489b05ed5fd31d34
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2022-05-03 21:04:54 +0100

    add filter proxy class

--- src/toolkit/audio/IFilter.h
+++ src/toolkit/audio/IFilter.h
@@ -1,7 +1,7 @@
 #ifndef TOOLKIT_AUDIO_IFILTER_H
 #define TOOLKIT_AUDIO_IFILTER_H
 
-#include <toolkit/audio/Buffer.h>
+#include <toolkit/audio/ISource.h>
 #include <memory>
 
 namespace TOOLKIT_NS { namespace audio
@@ -14,6 +14,24 @@ namespace TOOLKIT_NS { namespace audio
 		virtual void Process(float dt, FloatBuffer dst, ConstFloatBuffer src) = 0;
 	};
 	TOOLKIT_DECLARE_PTR(IFilter);
+
+	class Filter : public ISource
+	{
+	private:
+		IFilterPtr		_filter;
+		ISourcePtr		_source;
+
+	public:
+		Filter(const IFilterPtr &filter, const ISourcePtr & source):
+			_filter(filter), _source(source)
+		{ }
+
+		void Get(float dt, FloatBuffer buffer) override
+		{
+			_source->Get(dt, buffer);
+			_filter->Process(dt, buffer, buffer);
+		}
+	};
 
 }}
 
