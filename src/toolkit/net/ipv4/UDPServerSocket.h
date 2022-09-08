e466b39319946e1614436bb578e16341f7d93c5d
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2022-09-08 23:14:46 +0100

    add UDPSocket/ServerSocket

---
+++ src/toolkit/net/ipv4/UDPServerSocket.h
@@ -0,0 +1,21 @@
+#ifndef TOOLKIT_NET_IPV4_UDPSERVERSOCKET_H
+#define TOOLKIT_NET_IPV4_UDPSERVERSOCKET_H
+
+#include <toolkit/net/ipv4/UDPSocket.h>
+#include <toolkit/net/BaseServerSocket.h>
+
+namespace TOOLKIT_NS { namespace net { namespace ipv4
+{
+
+	class UDPServerSocket :
+		public BaseServerSocket<UDPSocket>
+	{
+	public:
+		UDPServerSocket(): BaseServerSocket(AF_INET, SOCK_DGRAM, 0)
+		{ }
+	};
+
+}}}
+
+
+#endif
---
+++ src/toolkit/net/ipv4/UDPSocket.h
@@ -0,0 +1,25 @@
+#ifndef TOOLKIT_NET_IPV4_UDPSOCKET_H
+#define TOOLKIT_NET_IPV4_UDPSOCKET_H
+
+#include <toolkit/net/BaseSocket.h>
+
+namespace TOOLKIT_NS { namespace net { namespace ipv4
+{
+	struct Endpoint;
+
+	class UDPSocket : public BaseSocket
+	{
+	public:
+		using BaseSocket::BaseSocket;
+
+		UDPSocket(): BaseSocket(AF_INET, SOCK_DGRAM, 0)
+		{ }
+
+		UDPSocket(UDPSocket && o): BaseSocket(std::move(o))
+		{ }
+	};
+
+}}}
+
+
+#endif
