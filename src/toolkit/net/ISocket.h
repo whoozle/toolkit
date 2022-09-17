1b18f291c46f37e4c1683f34820bde0813699c05
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2022-09-17 22:51:48 +0100

    add ISocketStream

--- src/toolkit/net/ISocket.h
+++ src/toolkit/net/ISocket.h
@@ -2,6 +2,7 @@
 #define TOOLKIT_NET_ISOCKET_H
 
 #include <toolkit/io/IPollable.h>
+#include <toolkit/io/IStream.h>
 #include <toolkit/core/Buffer.h>
 #include <toolkit/core/types.h>
 #include <memory>
@@ -17,6 +18,23 @@ namespace TOOLKIT_NS { namespace net
 		virtual ssize_t Recv(Buffer data, int flags) = 0;
 	};
 	TOOLKIT_DECLARE_PTR(ISocket);
+
+	struct ISocketStream :
+		public virtual ISocket,
+		public virtual io::IBidirectionalStream
+	{
+		virtual int GetReadFlags() const
+		{ return 0; }
+
+		virtual int GetWriteFlags() const
+		{ return 0; }
+
+		virtual size_t Read(Buffer data) override
+		{ return Recv(data, GetReadFlags()); }
+
+		virtual size_t Write(ConstBuffer data) override
+		{ return Send(data, GetWriteFlags()); }
+	};
 
 }}
 
