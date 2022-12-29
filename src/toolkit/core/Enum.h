e7a13b64b738ad7e17aed8ca373b5e3b97d86f02
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2022-12-29 11:16:23 +0000

    mark operators as constexpr

--- src/toolkit/core/Enum.h
+++ src/toolkit/core/Enum.h
@@ -5,7 +5,7 @@
 #include <type_traits>
 
 #define TOOLKIT_DECLARE_ENUM_BIT_OPERATOR(ENUM, OP) \
-	inline ENUM operator OP (ENUM a, ENUM b) \
+	inline constexpr ENUM operator OP (ENUM a, ENUM b) \
 	{ \
 		using T = std::underlying_type<ENUM>::type; \
 		return ENUM(static_cast<T>(a) OP static_cast<T>(b)); \
