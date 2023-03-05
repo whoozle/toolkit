c8e52f6b7372543ca88dc48f69f9ec9e31163dac
Vladimir Menshakov <vladimir.menshakov@gmail.com>
2023-03-05 11:58:54 +0000

    allow non-integral types for Hex()

--- src/toolkit/text/Formatters.h
+++ src/toolkit/text/Formatters.h
@@ -4,13 +4,20 @@
 #include <toolkit/text/StringOutputStream.h>
 #include <toolkit/core/Buffer.h>
 #include <string>
+#include <type_traits>
 
 namespace TOOLKIT_NS { namespace text
 {
+	inline char NibbleToHex(u8 v)
+	{ return v > 9? v + 'a' - 10: v + '0';}
+
 	class StringOutputStream;
 
+	template<typename ValueType, bool IntegralType = std::is_integral<ValueType>::value>
+	struct HexNumber;
+
 	template<typename ValueType>
-	struct HexNumber
+	struct HexNumber<ValueType, true>
 	{
 		ValueType 	Value;
 		unsigned	Width;
@@ -30,9 +37,7 @@ namespace TOOLKIT_NS { namespace text
 			size_t n;
 			for(n = Size; n; value >>= 4)
 			{
-				char ch = value & 0x0f;
-				ch = ch > 9? ch + 'a' - 10: ch + '0';
-				buf[--n] = ch;
+				buf[--n] = NibbleToHex(value & 0x0f);
 				if (value == 0 && Size - n >= width)
 					break;
 			}
@@ -42,7 +47,27 @@ namespace TOOLKIT_NS { namespace text
 	};
 
 	template<typename ValueType>
-	HexNumber<ValueType> Hex(ValueType value, unsigned padding = 0)
+	struct HexNumber<ValueType, false>
+	{
+		const ValueType & 	Value;
+		unsigned			Width;
+
+		HexNumber(const ValueType & value, unsigned width = 0):
+			Value(value), Width(width)
+		{ }
+
+		void ToString(StringOutputStream & ss) const
+		{
+			for(auto byte : Value) {
+				char buf[2] = { NibbleToHex(byte >> 4), NibbleToHex(byte & 0x0f) };
+				ss.Write(buf, 2);
+			}
+		}
+		TOOLKIT_DECLARE_SIMPLE_TOSTRING()
+	};
+
+	template<typename ValueType>
+	HexNumber<ValueType> Hex(const ValueType &value, unsigned padding = 0)
 	{ return HexNumber<typename std::decay<ValueType>::type> { value, padding }; }
 
 	struct HexDump
